d41008876d15d301fab3b97ddb4e76a3b6486317
Jeeson Domiguez <dominguez_js@outlook.com>
2020-07-31 00:49:39 -0600

    Primera parte del form creada y estilizada hasta 768

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment,  useState} from "react";
 import Header from "./Components/Header";
 
 function App() {
+    // States 
+    const [propiedad, savePropiedad] = useState({
+
+    });
+    // End OF States 
+
   return (
     <Fragment>
       <Header />
+      <div></div>
     </Fragment>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -11,6 +11,7 @@ import icon_s_util from "../img/icon-s-util.svg";
 import icon_s_total from "../img/icon-s-total.svg";
 import icon_dormitorios from "../img/icon-dormitorios.svg";
 import icon_banos from "../img/icon-banos.svg";
+import icon_estimar from "../img/icon-estimar.svg";
 
 // Styled Components
 const Formulario = styled.form`
@@ -33,6 +34,21 @@ const Formulario = styled.form`
     width: 25px;
     background: rgba(${myBlue}, 0.3);
     margin-bottom: 15px;
+  }
+
+  @media (min-width: 768px) {
+    padding: 50px 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    .section-title {
+      width: 100%;
+    }
+
+    .divider-container {
+      flex-basis: 100%;
+    }
   }
 `;
 
@@ -62,13 +78,23 @@ const FormGroup = styled.div`
     border-radius: 21px;
     outline: none;
     box-shadow: inset 10px 10px 15px rgba(${myInputShadow}, 0.5);
+
+    ::placeholder {
+      color: rgba(0, 0, 0, 0.4);
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 45%;
   }
 `;
 
 // END OF Styled Components
 
 const Form = () => {
+  // States
   const [listaComunas, saveListaComunas] = useState([]);
+  // End OF States
 
   //Consulta a API de comunas
   useEffect(() => {
@@ -95,7 +121,7 @@ const Form = () => {
         </strong>
       </p>
 
-      <div className="divider"></div>
+      <div className="divider-container"><div className="divider"></div></div>
 
       <FormGroup>
         <label className="required">
@@ -127,6 +153,74 @@ const Form = () => {
           ))}
         </select>
       </FormGroup>
+
+      <FormGroup>
+        <label className="required">
+          <img
+            src={icon_s_util}
+            alt="Superficie útil (m²)"
+            className="left-icon"
+          />
+          Superficie útil (m²)
+        </label>
+        <input
+          type="number"
+          name="superficie_util"
+          id="superficie_util"
+          placeholder="Ej. 110.00"
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <label className="required">
+          <img
+            src={icon_s_total}
+            alt="Superficie total (m²)"
+            className="left-icon"
+          />
+          Superficie total (m²)
+        </label>
+        <input
+          type="number"
+          name="superficie_total"
+          id="superficie_total"
+          placeholder="Ej. 136.15"
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <label className="required">
+          <img
+            src={icon_dormitorios}
+            alt="Numero de dormitorios"
+            className="left-icon"
+          />
+          Numero de dormitorios
+        </label>
+        <input
+          type="number"
+          name="dormitorios"
+          id="dormitorios"
+          placeholder="Ej. 4"
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <label className="required">
+          <img src={icon_banos} alt="Numero de banos" className="left-icon" />
+          Numero de banos
+        </label>
+        <input type="number" name="banos" id="banos" placeholder="Ej. 2" />
+      </FormGroup>
+
+      <button type="submit" className="primary-button submit-btn">
+        <img
+          src={icon_estimar}
+          alt="Estimar Precio de Venta"
+          className="left-icon"
+        />
+        Estimar Precio de Venta
+      </button>
     </Formulario>
   );
 };
