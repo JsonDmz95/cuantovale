437e8171bf59a248968aa79f0aa99ba5a1af9c6c
Jeeson Domiguez <dominguez_js@outlook.com>
2020-08-02 00:22:53 -0600

    Updates

---
+++ precache-manifest.eb45575e25b01e8b46da3ff1274a1bc6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41c84e0774e6da41caecec4333ee891",
+    "url": "/cuantovale/index.html"
+  },
+  {
+    "revision": "e72d82d88b7e623280ce",
+    "url": "/cuantovale/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "cb2653a7f74b3427286c",
+    "url": "/cuantovale/static/css/main.423a4b9f.chunk.css"
+  },
+  {
+    "revision": "e72d82d88b7e623280ce",
+    "url": "/cuantovale/static/js/2.f97e48b7.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/cuantovale/static/js/2.f97e48b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2653a7f74b3427286c",
+    "url": "/cuantovale/static/js/main.df8cb426.chunk.js"
+  },
+  {
+    "revision": "0c1abda3294acff82033",
+    "url": "/cuantovale/static/js/runtime-main.54d59b60.js"
+  },
+  {
+    "revision": "316625fb4a669456a5ad5ef8a1f643a9",
+    "url": "/cuantovale/static/media/collapse-angle.316625fb.svg"
+  },
+  {
+    "revision": "ce011ac5fac75a9c9d092d8eea9688aa",
+    "url": "/cuantovale/static/media/icon-banos-white.ce011ac5.svg"
+  },
+  {
+    "revision": "997a1ac2007b9c72c3873101d4f15218",
+    "url": "/cuantovale/static/media/icon-banos.997a1ac2.svg"
+  },
+  {
+    "revision": "34978a47333580545d919fc4af301770",
+    "url": "/cuantovale/static/media/icon-dormitorios-white.34978a47.svg"
+  },
+  {
+    "revision": "c01985efd4be4e5bd783173b0d5dfc75",
+    "url": "/cuantovale/static/media/icon-dormitorios.c01985ef.svg"
+  },
+  {
+    "revision": "ba15c64cb18a11d2c421618cbd84bd5e",
+    "url": "/cuantovale/static/media/icon-estimar.ba15c64c.svg"
+  },
+  {
+    "revision": "27e1e44f33c222b79babf771ff8d823c",
+    "url": "/cuantovale/static/media/icon-info-propiedad.27e1e44f.svg"
+  },
+  {
+    "revision": "0f6c9f5db4522fa47316434dd5b53b36",
+    "url": "/cuantovale/static/media/icon-s-total.0f6c9f5d.svg"
+  },
+  {
+    "revision": "42b73cd5b575bd2e25f952383786ab68",
+    "url": "/cuantovale/static/media/icon-s-util.42b73cd5.svg"
+  },
+  {
+    "revision": "cba4bcb94874751c45f1764348894728",
+    "url": "/cuantovale/static/media/icon-tipo.cba4bcb9.svg"
+  },
+  {
+    "revision": "81124bb557b0a541bf65c8e9956149d4",
+    "url": "/cuantovale/static/media/icon-ubicacion-white.81124bb5.svg"
+  },
+  {
+    "revision": "5aca20f01297aaa3801b5a31655f93ba",
+    "url": "/cuantovale/static/media/icon-ubicacion.5aca20f0.svg"
+  },
+  {
+    "revision": "a6639bd08bbae6925e314ec516e9246b",
+    "url": "/cuantovale/static/media/logo.a6639bd0.svg"
+  }
+]);
